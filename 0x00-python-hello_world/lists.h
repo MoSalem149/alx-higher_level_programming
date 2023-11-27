a027661eca83ff9c7c6d46782d52566b92bafa03
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-11-27 11:43:29 -0800

    10-check_cycle.c commit

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,3 +1,4 @@
+
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -7,7 +8,9 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
@@ -20,4 +23,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
