521fdd0df0a614a71bc23e9cb9824bfeac70b6cd
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-12-06 15:10:24 +0000

    12-roman_to_int.py commit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in roman_string] + [0]
+    result = 0
+
+    for num in range(len(numbers) - 1):
+        if numbers[num] >= numbers[num+1]:
+            result += numbers[num]
+        else:
+            result -= numbers[num]
+
+    return result
