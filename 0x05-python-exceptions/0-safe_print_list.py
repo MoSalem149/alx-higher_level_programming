a8a3ed55ae45a99200a22f5d85e3ea53f26b8c24
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-12-18 21:16:17 +0000

    3-safe_print_division.py commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 		try:
 			print("{}".format(my_list[i]), end="")
 			num += 1
-		except IndexError:
+		except (TypeError, IndexError):
 			None
 	print("")
 	return (num)
